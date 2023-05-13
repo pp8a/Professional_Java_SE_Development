8dc5f93fad3919dc650c23be72350d31d109d6fb
Alex <103247048+pp8a@users.noreply.github.com>
2023-05-13 11:17:42 +0300

    Add files via upload

--- Collections Framework/collections-count-words/src/main/java/com/epam/rd/autotasks/Words.java
+++ Collections Framework/collections-count-words/src/main/java/com/epam/rd/autotasks/Words.java
@@ -1,5 +1,6 @@
 package com.epam.rd.autotasks;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -7,56 +8,57 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.StringJoiner;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Words {
 
-    public String countWords(List<String> lines) {    
-
+    public String countWords(List<String> lines) { 
     	StringTokenizer tokenizer = new StringTokenizer(lines.toString(), "[] .,‘’(“—/:?!”;*)'\\\"-");    	
    	
     	Map<String, Integer> hm = new HashMap<String, Integer>();
     	
     	while(tokenizer.hasMoreTokens()) {
     		String str = tokenizer.nextToken().toLowerCase();
-    		if(hm.containsKey(str)) {
-    			Integer value = hm.get(str);
+//    		if(hm.containsKey(str)) {
+//    			Integer value = hm.get(str);
+//    			hm.put(str, ++value);
+//    		}else if(str.length() > 3){ //words which contain less than 4 letters    			
+//    			hm.put(str, 1);
+//    		}
+    		
+    		if(str.length() > 3) {
+    			//Возвращает значение, соответствующее ключу key. Если такой ключ не существует — возвращает значение по умолчанию.
+    			Integer value = hm.getOrDefault(str, 0);
     			hm.put(str, ++value);
-    		}else if(str.length() > 3){ //words which contain less than 4 letters    			
-    			hm.put(str, 1);
     		}
-    	}
-    	
-    	LinkedList<Entry<String, Integer>> list = new LinkedList<>(hm.entrySet());
-    	list.sort(new Comparator<Entry<String, Integer>>() {
+    	}    	
 
-			@Override
-			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
-				// sorted by their amount and then in alphabetical order
-				 if (o1.getValue().equals(o2.getValue())) {
-	                    return o1.getKey().compareTo(o2.getKey());
-	                }
-				return o2.getValue()-o1.getValue();
-			}
-		});
+    	List<Entry<String, Integer>> list = new ArrayList<>(hm.entrySet());
+//    	list.sort(new Comparator<Entry<String, Integer>>() {
+//
+//			@Override
+//			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+//				// sorted by their amount and then in alphabetical order
+//				 if (o1.getValue().equals(o2.getValue())) {
+//	                    return o1.getKey().compareTo(o2.getKey());
+//	                }
+//				return o2.getValue()-o1.getValue();
+//			}
+//		});
     	
-    	StringBuilder builder = new StringBuilder();    	
-    	boolean count = false;//for set "\n" if have still word corresponding to the condition
-    	for (Iterator<Entry<String, Integer>> iterator = list.iterator(); iterator.hasNext();) {
-			Entry<String, Integer> entry = iterator.next();
-			
-			if(entry.getValue() > 9 && count) {
-				builder.append("\n");
-			}			
-			
-			if(entry.getValue() > 9) {//words which appear less less than 10
-				builder.append(entry.getKey())
-					.append(" - ")
-					.append(entry.getValue());
-			}			
-			count = true;			
+    	list.sort(Map.Entry.<String, Integer>comparingByValue().reversed()
+                .thenComparing(Map.Entry.comparingByKey()));
+    	
+    	StringJoiner joiner = new StringJoiner("\n");
+    	for (Map.Entry<String, Integer> entry : list) {
+    		if(entry.getValue() > 9) {
+    			joiner.add(entry.getKey()+" - "+entry.getValue());
+    		}			
 		}
     	
-		return builder.toString();    	
+		return joiner.toString();    	
     }
 }
